1feb479ccfe15975e7929fe066d26f87af5dce7f
Hidekazu Miyamoto <hidekazu_miyamoto@fablic.co.jp>
2017-12-25 19:11:38 +0900

    debug

--- vars/config2.groovy
+++ vars/config2.groovy
@@ -1,13 +1,8 @@
 #!/usr/bin/env groovy
 
-import com.github.exelexe.jenkins.SampleConfig
-
-def jsonStr = libraryResource 'com/github/exelexe/jenkins/sample.json'
-def res = new SampleConfig(jsonStr)
-
 def getGitUrlFormat() {
-  echo jsonStr
-  echo res
-  //return res.getGitUrlFormat()
+  def jsonStr = libraryResource 'com/github/exelexe/jenkins/sample.json'
+  def res = new SampleConfig(jsonStr)
+  return res.getGitUrlFormat()
 }
 
