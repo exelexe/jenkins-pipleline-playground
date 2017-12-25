5b1661973667d2d06462ee545b2c72c4fb4c8754
Hidekazu Miyamoto <hidekazu_miyamoto@fablic.co.jp>
2017-12-25 17:15:32 +0900

    Bugfix param key

--- vars/config.groovy
+++ vars/config.groovy
@@ -4,6 +4,6 @@ def jsonStr = libraryResource 'com/github/exelexe/jenkins/sample.json'
 def config = new groovy.json.JsonSlurperClassic().parseText(jsonStr)
 
 def getGitUrlFormat() {
-  return config["git"]["fablic-url-format"]
+  return config["git"]["url-format"]
 }
 
