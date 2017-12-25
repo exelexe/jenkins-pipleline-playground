f04d719b2d93429402f6b2968de19cdc9a6f1a69
Hidekazu Miyamoto <hidekazu_miyamoto@fablic.co.jp>
2017-12-25 20:33:56 +0900

    try

--- vars/config2.groovy
+++ vars/config2.groovy
@@ -2,8 +2,8 @@
 import com.github.exelexe.jenkins.SampleConfig
 
 def jsonStr = libraryResource 'com/github/exelexe/jenkins/sample.json'
-def con = new SampleConfig(jsonStr)
+final MYCONSTANT = new SampleConfig(jsonStr)
 
 def getGitUrlFormat() {
-  return con.getGitUrlFormat()
+  return MYCONSTANT.getGitUrlFormat()
 }
