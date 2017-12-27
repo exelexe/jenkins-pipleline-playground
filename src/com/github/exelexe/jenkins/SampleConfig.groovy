093de5ba2375958eb3e59157f851e4918b74d9d9
Hidekazu Miyamoto <hidekazu_miyamoto@fablic.co.jp>
2017-12-27 15:22:00 +0900

    Add script args.

--- src/com/github/exelexe/jenkins/SampleConfig.groovy
+++ src/com/github/exelexe/jenkins/SampleConfig.groovy
@@ -1,15 +1,15 @@
 #!/usr/bin/env groovy
 package com.github.exelexe.jenkins
 
-import groovy.text.SimpleTemplateEngine
+//import groovy.text.SimpleTemplateEngine
 
 class SampleConfig implements Serializable {
 
   def res
 
-  public SampleConfig() {
+  public SampleConfig(Script script) {
     this.res = new groovy.json.JsonSlurperClassic().parseText(
-                       this.libraryResource('com/github/exelexe/jenkins/sample.json')
+                       script.libraryResource('com/github/exelexe/jenkins/sample.json')
 //                     this.class.getResource( 'com/github/exelexe/jenkins/sample.json').getText()
                    )
   }
--- vars/config2.groovy
+++ vars/config2.groovy
@@ -4,6 +4,6 @@ import com.github.exelexe.jenkins.SampleConfig
 def getGitUrlFormat() {
   def jsonStr = libraryResource 'com/github/exelexe/jenkins/sample.json'
   //final props = new SampleConfig(jsonStr)
-  props = new SampleConfig()
+  props = new SampleConfig(this)
   return props.getGitUrlFormat()
 }
