069fb823f6e096693ae8c1feec8983a735b52266
Hidekazu Miyamoto <hidekazu_miyamoto@fablic.co.jp>
2018-01-22 16:53:42 +0900

    Add tmp.

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,8 +1,16 @@
 #!/usr/bin/env groovy
+
+import groovy.transform.Field
 import com.github.exelexe.jenkins.SampleConfig
+
+@Field def CONF = new EnvironmentConfig(this)
 
 def getGitUrlFormat() {
   conf = new SampleConfig(this)
   return conf.getGitUrlFormat()
+}
+
+def getGitUrlFormatTmp() {
+  return CONF.getGitUrlFormat()
 }
 
